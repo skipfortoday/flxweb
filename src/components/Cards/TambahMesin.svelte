b54886e88e5b46e9db6a7be0dbcb8bcfe17b0930
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-05 09:51:59 +0700

    Mennambah List User

--- src/components/Cards/TambahMesin.svelte
+++ src/components/Cards/TambahMesin.svelte
@@ -1,3 +1,13 @@
+<script>
+  export let pegawai
+  $: fetch(`http://192.168.0.25:3000/api/user`)
+		.then(r => r.json())
+		.then(user => {
+			pegawai = user
+		}); 
+</script>
+
+
 <div
   class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200 border-0"
 >
@@ -52,5 +62,112 @@
         </div>
       </div>
     </form>
+  </div>  
+</div>
+{#if pegawai}
+<div
+  class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+>
+  <div class="rounded-t mb-0 px-4 py-3 border-0">
+    <div class="flex flex-wrap items-center">
+      <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+        <h3 class="font-semibold text-base text-gray-800">
+          List Pegawai
+        </h3>
+      </div>
+      <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+        <button
+          class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          See all
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="block w-full overflow-x-auto">
+    <!-- Projects table -->
+    <table class="items-center w-full bg-transparent border-collapse">
+      <thead>
+        <tr>
+          <th
+          class="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left"
+          >
+          No
+          </th>
+          <th
+            class="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left"
+          >
+            Nama
+          </th>
+          <th
+            class="px-6 bg-gray-100 text-gray-600 align-middle border border-solid border-gray-200 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-no-wrap font-semibold text-left"
+          >
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each pegawai as pegawai, i}
+        <tr>
+          <th
+            class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4 text-left"
+          >
+           {[i+1]}
+          </th>
+          <td
+            class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4"
+          >
+              {pegawai.Nama}
+          </td>
+          <td
+          class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-no-wrap p-4"
+           >
+          </td>
+        </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+  <div class="py-2">
+    <nav class="block">
+      <ul class="flex pl-0 rounded list-none flex-wrap">
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            <i class="fas fa-chevron-left -ml-px"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 text-white bg-orange-500">
+            1
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            2
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            3
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            4
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            5
+          </a>
+        </li>
+        <li>
+          <a href="#pablo" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-orange-500 bg-white text-orange-500">
+            <i class="fas fa-chevron-right -mr-px"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </div>
+{/if}
