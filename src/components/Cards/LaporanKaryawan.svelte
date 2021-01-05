548bce1aee2da4ff00aa288c422367b6bc139aeb
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-05 16:02:29 +0700

    Laporan Karyawan Tampilan

--- src/components/Cards/LaporanKaryawan.svelte
+++ src/components/Cards/LaporanKaryawan.svelte
@@ -13,16 +13,23 @@
   >
     <div class="rounded-t bg-white mb-0 px-6 py-6">
       <div class="text-center flex justify-between">
-        <h6 class="text-gray-800 text-xl font-bold">List Scan Karyawan @Rizqi Pratama</h6>
+        <h6 class="text-gray-800 text-xl font-bold">Laporan Absensi @Rizqi Pratama</h6>
+        <button
+        class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+        type="button"
+      >
+      <i class="fas fa-print"></i> Print
+      </button>
       </div>
+      
     </div>
     <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
       <form>
         <h6 class="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
-          Filter Sesuai Tanggal untuk Cetak Laporan
+          tentukan Tanggal Awal Dan Akhir sebelum cetak laporan
         </h6>
         <div class="flex flex-wrap">
-          <div class="w-full lg:w-4/12 px-4">
+          <div class="w-full lg:w-3/12 px-4">
             <div class="relative w-full mb-3">
               <label
                 class="block uppercase text-gray-700 text-xs font-bold mb-2"
@@ -63,10 +70,17 @@
                 placeholder="Tanggal Akhir"
               />
             </div>
+         </div>
+         <div class="w-full lg:w-3/12 px-4">
+          <div class="relative w-full mb-3">
+           <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-orange-600 bg-orange-200 uppercase last:mr-0 mr-1">
+                  Lihat Laporan
+          </span>
+          <button class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+            <i class="fas fa-book-open"></i> Data Scan
+          </button>
           </div>
-          <button class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-            Lihat Laporan
-         </button>
+          </div>
         </div>          
       </form>
     </div>  
