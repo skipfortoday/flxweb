9381628cf20e3d9416c3398e843f235912e7af19
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-12 09:37:28 +0700

    Fix Menu Laporan Karyawan

--- src/components/Cards/LaporanKaryawan.svelte
+++ src/components/Cards/LaporanKaryawan.svelte
@@ -1,6 +1,6 @@
 <script>
-  export let recentscan 
-  import print from 'print-js'
+  export let recentscan
+ 
 
   $: fetch(`http://192.168.1.100:3000/api/tlog/SB1MIT005&2020-12-08&2021-01-1`)
 		.then(r => r.json())
@@ -11,7 +11,7 @@
 
 {#if recentscan}
 <div
-  class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded" id="printJS-form"
+  class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
 >
   <div class="rounded-t mb-0 px-4 py-3 border-0">
     <div class="flex flex-wrap items-center">
@@ -21,7 +21,7 @@
         </h3>
       </div>
       <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-        <button class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="printJS('printJS-form', 'html'>
+        <button class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
           <i class="fas fa-print"></i> Print
         </button>
       </div>
